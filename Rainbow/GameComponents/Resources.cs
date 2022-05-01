47a33eb2d466ea78eeeb685a08fd3c8dcb24fcac
ThousandCuts-0064 <92256555+ThousandCuts-0064@users.noreply.github.com>
2022-05-01 22:10:27 +0300

    Pre birdy animation

--- Rainbow/GameComponents/Resources.cs
+++ Rainbow/GameComponents/Resources.cs
@@ -11,15 +11,44 @@ namespace Rainbow
     public static class Resources
     {
         private static readonly string _folder;
+        private static readonly Image[] _birdyAnimation;
         public static Image ColorWheel { get; }
         public static Image Birdy { get; }
+        public static Image[] BirdyAnimation => _birdyAnimation.ToArray();
 
         static Resources()
         {
             _folder = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName + $"\\{nameof(Resources)}\\";
 
-            ColorWheel = Image.FromFile($"{_folder}{nameof(ColorWheel)}.png");
-            Birdy = Image.FromFile($"{_folder}{nameof(Birdy)}.jpg");
+            ColorWheel = LoadImage($"{nameof(ColorWheel)}.png");
+            Birdy = LoadImage($"{nameof(Birdy)}.jpg");
+            _birdyAnimation = BirdyImageToAnimation(LoadImage($"{nameof(BirdyAnimation)}.png"));
+        }
+
+        private static Image LoadImage(string name) =>
+            Image.FromFile(_folder + name);
+
+        private static Image[] BirdyImageToAnimation(Image image)
+        {
+            Bitmap bitmap = new Bitmap(image);
+            Image[] images = new Image[25];
+            int rows = 3;
+            int columns = 9;
+            Size single = new Size(image.Width / columns, image.Height / rows);
+            Rectangle current = new Rectangle(new Point(0, 0), single);
+            for (int c = 0; c < columns - 1; c++)
+            {
+                for (int r = 0; r < rows; r++)
+                {
+                    images[c * rows + r] = bitmap.Clone(current, image.PixelFormat);
+                    current.Location.Offset(0, single.Height);
+                }
+                current.Offset(single.Width, 0);
+                current.Y = 0;
+            }
+            images[images.Length - 1] = bitmap.Clone(current, image.PixelFormat);
+
+            return images;
         }
     }
 }
---
+++ Rainbow/GameObject/OtherTypes/Animation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rainbow
+{
+    class Animation : GameObject
+    {
+        private readonly Image[] _images;
+        public int Index { get; private set; }
+        public RectangleF Rectangle { get; set; }
+
+        public Animation(Image[] images, RectangleF rectangle, Layer layer) : base(layer)
+        {
+            _images = images;
+            Rectangle = rectangle;
+        }
+
+        public override void Draw(Graphics graphics) => 
+            graphics.DrawImage(_images[Index == _images.Length ? Index = 0 : Index++], Rectangle);
+    }
+}
